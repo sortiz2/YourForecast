ae4cd5855464f70f3ae44974856cf59588e5526d
sortiz2 <sortiz2@live.maryville.edu>
2020-11-15 19:12:37 -0500

    Sprint 1 Update

---
+++ index.js
@@ -0,0 +1,52 @@
+const searchForm = document.querySelector('.search-location');
+const cityValue = document.querySelector('.search-location input');
+const cityName= document.querySelector('.city-name p');
+const cardBody = document.querySelector('.card-body');
+
+const F =(kelvin)=>{
+    farh = Math.round((kelvin - 273.15) * 9/5 + 32);
+    return farh;
+}
+
+updateWeather = (city)=>{
+    console.log(city);
+    cityName.textContent = city.name;
+    cardBody.innerHTML = `
+    <div class="card body" > 
+        <div class="card-mid row">
+                <div class="col text-center temp">
+                    <span>${F(city.main.temp)}&deg;F</span>
+                </div>
+                <div class="col condition-temp">
+                    <p class="condition">${city.weather[0].description}</p>
+                    <p class="high">${F(city.main.temp_max)}&deg;F</p>
+                    <p class="low">${F(city.main.temp_min)}&deg;F</p>
+                </div>         
+            </div>
+            <div class="icon-container card shadow mx-auto">
+                <img src="img/cloud.png" class="card-img-icon" alt=""/>
+            </div>
+            <div class="card-bottom px-5 py-4 row">
+                <div class="col text-center">
+                    <p>${F(city.main.feels_like)}&deg;F</p>
+                    <span>Feels Like</span>
+                </div>
+                <div class="col text-center">
+                    <p>${(city.main.humidity)}%</p>
+                    <span>Humidity</span>
+                    </div>
+                </div>`;
+}
+
+searchForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const citySearch = cityValue.value.trim();
+    console.log(citySearch);
+    searchForm.reset();
+
+    requestCity(citySearch)
+        .then((data)=>{
+            updateWeather(data);
+        }) 
+        .catch((error)=>{console.log(error)})
+})
---
+++ request.js
@@ -0,0 +1,12 @@
+const key = '594e90ce02964b4bf7d942ed63b3f6ca';
+
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    const response = await fetch(baseURL + query);
+
+    const data = await response.json();
+    return data;
+
+}
